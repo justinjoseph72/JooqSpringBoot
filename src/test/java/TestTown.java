112e0fff89c8f93031d4461b7561b4a2f581ca8c
Justin Joseph <jjo@commsoft.co.uk>
2017-04-17 22:28:55 +0100

    updating pom to add empty relativeparent

---
+++ src/test/java/TestTown.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import repo.TownRepo;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Justin on 17/04/2017.
+ */
+public class TestTown {
+
+    @Test
+    public  void testTowns(){
+        assertEquals(1, 1);
+    }
+}
